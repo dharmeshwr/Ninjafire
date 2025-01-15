966432ffb5c03e9292a73e88a989c83c5eabaa6a
Dharmesh53 <dharmeshwr@gmail.com>
2025-01-16 01:52:23 +0530

    fix: og image content type

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -2,8 +2,6 @@ import { ImageResponse } from "next/og";
 
 export const runtime = "edge";
 
-export const contentType = "image/png";
-
 export async function GET() {
   try {
     const train = await fetch(
