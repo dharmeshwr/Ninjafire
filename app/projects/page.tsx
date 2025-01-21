78921842a4b78f3bd250220b49e09160a979fac2
Dharmesh53 <dharmeshwr@gmail.com>
2025-01-22 01:29:44 +0530

    refactor: projects filters on search params instead of state

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,36 +1,34 @@
 "use client";
 
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import { projects, type Project } from "@/config/projects";
-import { capatilize, cn } from "@/lib/utils";
+import { capatilize, cn, createQueryString } from "@/lib/utils";
 
-type CategoryType = "type" | "date";
-
-function sortProjectsByYear(
-  projects: Record<string, Project[]>,
-): Record<string, Project[]> {
-  const sortedKeys = Object.keys(projects).sort(
-    (a, b) => Number(b) - Number(a),
-  );
-
-  return sortedKeys.reduce(
-    (acc, key) => {
-      acc[`Year ${key}`] = projects[key];
-      return acc;
-    },
-    {} as Record<string, Project[]>,
-  );
+enum CategoryType {
+  DATE = "date",
+  TYPE = "type",
 }
 
+const Categories = [CategoryType.TYPE, CategoryType.DATE];
+
 export default function ProjectsPage() {
-  const [category, setCategory] = useState<CategoryType>("type");
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchparams = useSearchParams();
+  let category =
+    (searchparams.get("category") as CategoryType) ?? CategoryType.TYPE;
+
+  if (!Categories.includes(category)) {
+    category = CategoryType.TYPE;
+  }
 
   const categorizedProjects = useMemo(() => {
     const grouped = projects.reduce<Record<string, Project[]>>(
       (accumulate, current) => {
         const key =
-          category === "type"
+          category === CategoryType.TYPE
             ? current[category]
             : current[category][0].split(" ")[1]; // Extract year from date
 
@@ -43,7 +41,9 @@ export default function ProjectsPage() {
       {},
     );
 
-    return category === "date" ? sortProjectsByYear(grouped) : grouped;
+    return category === CategoryType.DATE
+      ? sortProjectsByYear(grouped)
+      : grouped;
   }, [category]);
 
   return (
@@ -52,14 +52,20 @@ export default function ProjectsPage() {
         <h2 className="text-2xl">My Projects</h2>
 
         <div className="flex justify-center rounded-full border border-foreground/40 bg-foreground/5 p-1 text-sm font-semibold">
-          {["type", "date"].map((key) => (
+          {Categories.map((key) => (
             <button
               key={key}
               className={cn(
                 "inline-flex items-center rounded-full px-2 py-1",
                 category === key && "bg-foreground text-background",
               )}
-              onClick={() => setCategory(key as CategoryType)}
+              onClick={() =>
+                router.push(
+                  pathname +
+                    "?" +
+                    createQueryString("category", key, searchparams),
+                )
+              }
             >
               {capatilize(key)}
             </button>
@@ -97,4 +103,15 @@ function ProjectList({ data }: { data: Record<string, Project[]> }) {
       })}
     </div>
   );
+}
+
+function sortProjectsByYear(projects: Record<string, Project[]>) {
+  const sortedKeys = Object.keys(projects).sort(
+    (a, b) => Number(b) - Number(a),
+  );
+
+  return sortedKeys.reduce((acc, key) => {
+    acc[`Year ${key}`] = projects[key];
+    return acc;
+  }, {});
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -5,9 +5,19 @@ export const cn = (...classes: ClassValue[]) => {
   return twMerge(clsx(classes));
 };
 
-export function capatilize(string: string) {
+export const createQueryString = (
+  name: string,
+  value: string,
+  existingSearchParams: any,
+) => {
+  const params = new URLSearchParams(existingSearchParams.toString());
+  params.set(name, value);
+  return params.toString();
+};
+
+export const capatilize = (string: string) => {
   return string[0].toUpperCase() + string.slice(1);
-}
+};
 
 export const formatDate = (input: string | number): string => {
   const date = new Date(input);
