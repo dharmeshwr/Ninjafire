e5f6489d8d835ab6358cfdbf4f07a39446b4bb1e
Dharmesh53 <dharmeshwr@gmail.com>
2025-01-28 00:18:24 +0530

    feat: add playground

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,7 +15,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     lastModified: new Date().toISOString().split("T")[0],
   }));
 
-  const routes = ["", "blogs", "projects"].map((route) => ({
+  const routes = ["", "blogs", "projects", "playground"].map((route) => ({
     url: `${BaseUrl}${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
