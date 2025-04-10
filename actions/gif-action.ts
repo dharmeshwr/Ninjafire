e47f292958bc5635b447633b6367103a7ca39f3f
Dharmesh53 <dharmeshwr@gmail.com>
2025-04-10 11:37:17 +0530

    refactor: change gif provider from giphy to tenor

--- actions/gif-action.ts
+++ actions/gif-action.ts
@@ -8,13 +8,22 @@ export const getGIF = async () => {
   const selectedKeywords = keywords[Math.floor(Math.random() * 4)];
 
   try {
+    //TENOR GIFS
     const { data } = await axios.get(
-      `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=${selectedKeywords}&rating=pg-13`,
+      `https://tenor.googleapis.com/v2/search?q=${selectedKeywords}&key=${process.env.TENOR_API_KEY}&client_key=Ninjafire&random=true&limit=1&contentfilter=off`,
     );
-    return { gif: data.data.images.original.webp };
+    return { gif: data.results[0].media_formats.tinygif.url };
 
-    // GIPHY API allows only 100 requests per hour, so during dev. please use this static gif
-    // return { gif: "https://media0.giphy.com/media/v1.Y2lkPWZlMzI0YWYzZXRmN290eDZrajJycmk2bXdnaXVrdTZ6NWpnYm1sc253ZW5maTgyOCZlcD12MV9naWZzX3JhbmRvbSZjdD1n/ff54BctkWA5ry/giphy.webp" }
+    // GIPHY GIFS
+    /*
+      const { data } = await axios.get(
+         `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=${selectedKeywords}&rating=pg-13`,
+       );
+       return { gif: data.data.images.original.webp };
+
+       GIPHY API allows only 100 requests per hour, so during dev. please use this static gif
+       return { gif: "https://media0.giphy.com/media/v1.Y2lkPWZlMzI0YWYzZXRmN290eDZrajJycmk2bXdnaXVrdTZ6NWpnYm1sc253ZW5maTgyOCZlcD12MV9naWZzX3JhbmRvbSZjdD1n/ff54BctkWA5ry/giphy.webp" }
+    */
   } catch (error) {
     return { error };
   }
