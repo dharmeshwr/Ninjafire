dfbcc54f8c0d919f9843febd3f2fda15aad2bbb4
Dharmeshwr <dharmeshwr@gmail.com>
2025-07-06 12:06:02 +0530

    fix: multiple jotai instance error

--- store/index.ts
+++ store/index.ts
@@ -2,6 +2,8 @@ import { atom } from "jotai";
 
 import { getMyAge } from "@/lib/utils";
 
+const age = getMyAge();
+
 export const globalZIndexAtom = atom(1000);
 export const hoverStateAtom = atom(false);
-export const ageAtom = atom(getMyAge());
+export const ageAtom = atom(age);
